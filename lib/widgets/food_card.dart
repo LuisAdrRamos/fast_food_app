0630e94d6fca59337a890c66fcde865d697afd2b
Luis Adrian Ramos <luchoramos042004@gmail.com>
2025-11-29 14:37:31 -0500

    values

--- lib/widgets/food_card.dart
+++ lib/widgets/food_card.dart
@@ -22,7 +22,7 @@ class FoodCard extends StatelessWidget {
         borderRadius: BorderRadius.circular(15),
         boxShadow: [
           BoxShadow(
-            color: Colors.grey.withOpacity(0.3),
+            color: Colors.grey.withValues(alpha: 0.3),
             blurRadius: 5,
             offset: const Offset(0, 3),
           )
